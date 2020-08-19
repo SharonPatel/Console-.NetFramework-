cfde05167ca6825b343ff7536df3c90736f59d29
991503002 <patsharo@sheridancollege.ca>
2020-08-19 17:50:42 -0400

    Final

---
+++ Methods.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Core.Common.CommandTrees;
+using System.Data.Entity.Validation;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalEFSharonPatel
+{
+    class Methods
+    {
+
+      
+        public static void getAllAuthors()
+        {
+            var context = new BooksDBEntities();
+            var selectQuery = (from a in context.Authors
+                               select a).ToList();
+
+            Console.WriteLine("==================================================================================================================\n\n");
+            foreach (var select in selectQuery)
+            {
+                Console.WriteLine($"{select.AuthorID,10} {select.FirstName,-20} " +
+                    $"{select.LastName,-20 } {select.Phone, -20} {select.Address,-20}" +
+                    $"{select.City,-20} {select.State,-20} ");
+            }
+        }
+
+        public static void AddAuthors()
+        {
+            using (var context = new BooksDBEntities())
+            {
+                try
+                {
+                    var aut = new Author();
+
+
+                    Console.WriteLine("Enter Author FirstName:");
+                    var input = Console.ReadLine();
+
+
+                    Console.WriteLine("Enter Author LasttName:");
+                    string input1 = Console.ReadLine();
+
+                    Console.WriteLine("Enter Author PhoneDetails:");
+                    string inputPhone = Console.ReadLine();
+
+                    Console.WriteLine("Enter Address:");
+                    string inputAddress = Console.ReadLine();
+
+                    Console.WriteLine("Enter City:");
+                    string inputCity = Console.ReadLine();
+
+                    Console.WriteLine("Enter State:");
+                    string inputState = Console.ReadLine();
+
+
+                    aut.FirstName = input;
+                    aut.LastName = input1;
+                    aut.Phone = inputPhone;
+                    aut.Address = inputAddress;
+                    aut.City = inputCity;
+                    aut.State = inputState;
+
+
+                    context.Authors.Add(aut);
+                    Console.WriteLine("Addedd Successfully");
+
+                    context.SaveChanges();
+
+
+                }
+                catch (DbEntityValidationException e)
+                {
+                    Console.WriteLine(e);
+                   
+                    throw;
+                }
+                }
+        }
+
+        public static void updateAuthor()
+        {
+            using (var context = new BooksDBEntities())
+            {
+                try
+                {
+                   
+
+                    //var try1= new Author();
+
+                    Console.WriteLine("Enter Id to search:");
+                    int id = Convert.ToInt32(Console.ReadLine());
+                    var upd = context.Authors.Find(id);
+
+                   
+                        if (upd != null)
+                        {
+                            Console.WriteLine("Enter Author FirstName");
+                            string fname = Console.ReadLine();
+
+
+                            Console.WriteLine("Enter Author LastName");
+                            string lname = Console.ReadLine();
+
+                            Console.WriteLine("Enter Phone Details:");
+                            string phone = Console.ReadLine();
+
+                            Console.WriteLine("Enter Address:");
+                            string address = Console.ReadLine();
+
+                            Console.WriteLine("Enter City:");
+                            string city = Console.ReadLine();
+
+                            Console.WriteLine("Enter State:");
+                            string state = Console.ReadLine();
+
+
+
+                            upd.FirstName = fname;
+                            upd.LastName = lname;
+                            upd.Phone = phone;
+                            upd.Address = address;
+                            upd.City = city;
+                            upd.State = state;
+                        
+                        Console.WriteLine("Inserted Successfully");
+
+                      }
+                        else
+                        {
+                             Console.WriteLine("Id doesn't exist"); 
+                        }
+                        context.SaveChanges();
+
+                }
+                catch (DbEntityValidationException e)
+                {
+                    Console.WriteLine(e);
+                    throw;
+                }
+
+            }
+        }
+      
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalEFSharonPatel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+            bool check = false; //used for do While Condition
+            var choice1 = 0;  //Taking choice for Main Menu
+            do
+            {
+                try
+                {
+                    //Main Menu
+                    Console.WriteLine("==================================================================================================================\n\n");
+                    Console.WriteLine("Welcome Please Choose a Command");
+                    Console.WriteLine("Press 1 to View all Authors");
+                    Console.WriteLine("Press 2 to Add Authors");
+                    Console.WriteLine("Press 3 to Update Authors");
+                    Console.WriteLine("Press 4 to Exit");
+
+                    choice1 = Convert.ToInt32(Console.ReadLine());
+                   
+
+
+                    switch (choice1)
+                    {
+                        case 1:
+
+                            Methods.getAllAuthors();
+                                break;
+                        case 2:
+
+                            Methods.AddAuthors();
+                            break;
+
+                        case 3:
+
+                            Methods.updateAuthor();
+                            break;
+                        case 4:
+                            Environment.Exit(0);
+                            break;
+
+
+                    }
+
+
+
+
+                }
+                catch (FormatException)
+                {
+
+                    Console.WriteLine("InValid Input! Please Enter correct Data to processed");
+                }
+            } while (!check);
+        }
+    }
+}
